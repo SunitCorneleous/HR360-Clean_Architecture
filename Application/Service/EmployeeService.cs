6fa2c742ea84209df068e52e18f4be40a4e4adf9
Sunit Corneleous <sunit.corneleous25@gmail.com>
2025-08-21 16:23:43 +0600

    Add project files.

---
+++ Application/Service/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Service
+{
+    public class EmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployeesAsync()
+        {
+            return await _employeeRepository.GetAllEmployeesAsync();
+        }
+    }
+}
---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Department { get; set; } = String.Empty;
+        public decimal Salary { get; set; }
+    }
+}
